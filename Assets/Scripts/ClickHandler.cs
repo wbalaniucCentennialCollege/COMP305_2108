089d171393aab747ac013a19aab22b7d0ce0182d
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-01-30 23:21:17 -0500

    Initial commit

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+
+    public Sprite sprite;
+    public SpriteRenderer sRend;
+
+    private void OnMouseUpAsButton()
+    {
+        sRend.sprite = sprite;
+    }
+}
