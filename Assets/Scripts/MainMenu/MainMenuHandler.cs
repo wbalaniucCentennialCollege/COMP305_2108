f381aa8e027c84b0a986b83ed782f45f2843877d
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-03-02 12:26:50 -0500

    Added main menu scene

---
+++ Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour {
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Week6_2");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
